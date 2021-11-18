c308d2a07af6d6e2959f3a44dda14c4cb2b1627b
rickyaditb <rickyaditb@gmail.com>
2021-11-18 19:03:58 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+      textColor: ['checked'],
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms')
+  ]
+}
